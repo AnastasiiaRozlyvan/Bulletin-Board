36ff826ac08cd25ef5ba725e3a905f010a6a8447
stacy <nastia.2.r.2@gmail.com>
2019-07-06 16:49:46 +0300

    step 6: Added model of rubric

--- models.py
+++ models.py
@@ -25,3 +25,16 @@ class Ad(db.Model):
 
     def __repr__(self):
         return f'<Ad id: {self.id}, title: {self.title}>'
+
+
+class Rubric(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Rubric, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return f'<Rubric id: {self.id}, name {self.name}>'

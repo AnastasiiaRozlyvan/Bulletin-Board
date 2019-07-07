4c13fcec09cf8f6aa36be7e55e1ba7d4ab1aeca5
stacy <nastia.2.r.2@gmail.com>
2019-07-07 19:06:31 +0300

    step 8: Search and forms added

--- ads/blueprint.py
+++ ads/blueprint.py
@@ -2,15 +2,28 @@ from flask import Blueprint
 from flask import render_template
 
 from models import Ad, Rubric
+from .forms import AdForm
+
+from flask import request
 
 
 ads = Blueprint("ads", __name__, template_folder="templates")
 
 
+@ads.route('/create')
+def create_ad():
+    form = AdForm()
+    return render_template('create_ad.html', form=form)
+
+
 @ads.route("/")
 def index():
-    ads = Ad.query.all()
-    return render_template("ads/index.html", ads=ads)
+    q = request.args.get('q')
+    if q:
+        adv = Ad.query.filter(Ad.title.contains(q) | Ad.body.contains(q)).all()
+    else:
+        adv = Ad.query.all()
+    return render_template("ads/index.html", ads=adv)
 
 
 @ads.route("/<slug>")
@@ -25,3 +38,4 @@ def rubric_detail(slug):
     rubric = Rubric.query.filter(Rubric.slug == slug).first()
     adv = rubric.ads
     return render_template("ads/rubric_detail.html", rubric=rubric, ads=adv)
+

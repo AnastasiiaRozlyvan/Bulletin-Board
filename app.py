0085bcf739372a3ca3e1a949a847a855e1dd5b7b
stacy <nastia.2.r.2@gmail.com>
2019-07-09 18:31:58 +0300

    step 14: Administration feature added.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask import redirect, url_for, request
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_security import Security
+from flask_security import current_user
 
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_admin import AdminIndexView
 
 from flask_security import SQLAlchemyUserDatastore
 
@@ -18,17 +21,45 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+manager.add_command("db", MigrateCommand)
 
 
 from models import *
 
 
-admin = Admin(app)
-admin.add_view(ModelView(Ad, db.session))
-admin.add_view(ModelView(Rubric, db.session))
+class AdminMixin:
+    def is_accessible(self):
+        return current_user.has_role("admin")
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("security.login", next=request.url))
+
+
+class BaseModelView(ModelView):
+    def on_model_change(self, form, model, is_created):
+        model.generate_slug()
+        return super(BaseModelView, self).on_model_change(form, model, is_created)
+
+
+class AdminView(AdminMixin, ModelView):
+    pass
+
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+    pass
+
+
+class AdAdminView(AdminMixin, BaseModelView):
+    form_columns = ['title', 'body', 'rubrics']
+
+
+class RubricAdminView(AdminMixin, BaseModelView):
+    form_columns = ['name', 'ads']
+
+
+admin = Admin(app, "B_board", url="/", index_view=HomeAdminView(name="Home"))
+admin.add_view(AdAdminView(Ad, db.session))
+admin.add_view(RubricAdminView(Rubric, db.session))
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
-
-
